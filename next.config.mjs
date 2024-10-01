c1e4ea19f8a17e58253d0b0eb1152e1edc565b1d
Syed_Basit_ <121909923+syed-basit7@users.noreply.github.com>
2024-10-01 16:17:09 +0500

    project upladed

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      BASE_URL: "https://dev.system.simka.solutions/",
+      API_KEY : "R6ql1aHq.cjzKA8WDwL7qMHZB9B6WSGydDz46sgmA"
+    }
+  };
+  
+  export default nextConfig;
+  
