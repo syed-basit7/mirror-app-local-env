784d3b5d3f5db4ca37920361d8b6288cd51e4d38
Al Amin <68145918+alminAl@users.noreply.github.com>
2024-10-02 10:06:31 +0000

    check enimate

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Hello World,</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
